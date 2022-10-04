8339c2d10e2dcbe73141fc40a2b047287a15ab2b
antoniojunior52 <85879690+antoniojunior52@users.noreply.github.com>
2022-10-04 11:41:41 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Número Divisível</title>
+</head>
+<body>
+  
+    <form action="calc.php" method="post">
+           <H1>
+                Digite o Número:
+           </H1>     
+
+        <input type="text" name="numero">
+        <br>
+        <input type="submit" value="Confimar">
+
+    </form>
+    
+</body>
+</html>
