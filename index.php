9352efc6fff68c1fc45b72a67c78d96a2c843355
e d u a r d a <86473064+eduardarn@users.noreply.github.com>
2022-10-04 12:13:41 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Número Divisível</title>
+    <title>Número e ETC</title>
 </head>
 <body>
   
@@ -12,6 +12,7 @@
            <H1>
                 Digite o Número:
            </H1>     
+        <P> AAAAYGUDATGWDEXYWAYBGDYQAWHGSDEHGWA </P>
 
         <input type="text" name="numero">
         <br>
